e00db69bd593a512a584fa49a43f6990e2e1378d
Hugo-walando <hugowalando@gmail.com>
2024-06-04 16:00:44 +0200

    feat: add creation of groups

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 let groupNumberSelect = document.querySelector('#group-number');
-let memberName = document.querySelector('#member-name');
+const memberName = document.querySelector('#member-name');
+const groupContainer = document.querySelector('#group-container');
+let submitBtn = document.querySelector('#submit-btn');
+let groupNumber = groupNumberSelect.value;
 
 groupNumberSelect.addEventListener('change', (e) => {
-  let groupNumber = e.currentTarget.value;
+  groupNumber = e.currentTarget.value;
   console.log(groupNumber);
+});
+
+submitBtn.addEventListener('click', (e) => {
+  groupContainer.innerHTML = '';
+  e.preventDefault();
+  for (let i = 1; i <= groupNumber; i++) {
+    let groupDiv = document.createElement('div');
+    groupDiv.className = 'p-3 border rounded-md w-max';
+
+    let groupTitleNumber = document.createElement('h2');
+    groupTitleNumber.textContent = `Groupe ${i}`;
+
+    let memberGroup = document.createElement('li');
+    memberGroup.textContent = memberName.value;
+
+    groupDiv.appendChild(groupTitleNumber);
+    groupDiv.appendChild(memberGroup);
+    groupContainer.appendChild(groupDiv);
+  }
 });
